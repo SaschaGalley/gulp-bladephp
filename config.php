b98b1a7b101dc9fbf36ecf8ddb98acfcaf57b909
Sascha Galley <sg@seso.at>
2014-03-20 10:22:16 +0100

    build setup

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php return array(
+
+    'views' => $cwd.'/source/templates',
+    'cache' => $cwd.'/cache',
+
+    'boot' => array(),
+
+    'global_variables' => array(),
+
+    'template_variables' => array(),
+
+);
